ad75408e68a44305da4b2bb968a21ab78a6957cc
aserra92 <aria.serrafelip@gmail.com>
2019-02-24 20:22:49 +0100

    docking

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -11,17 +11,17 @@ public:
 
 	void OnUpdate() override
 	{
-		if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
-			HZ_TRACE("Tab key is pressed!!");
+		/*if (Hazel::Input::IsKeyPressed(HZ_KEY_TAB))
+			HZ_TRACE("Tab key is pressed!!");*/
 	}
 
 	void OnEvent(Hazel::Event& event) override
 	{
-		if (event.GetEventType() == Hazel::EventType::KeyPressed)
+		/*if (event.GetEventType() == Hazel::EventType::KeyPressed)
 		{
 			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
 			HZ_TRACE("{0}", (char)e.GetKeyCode());
-		}
+		}*/
 	}
 };
 
