806ec9ab61cc7f8c22299933f9f1b035ea9bf4fa
aserra92 <aria.serrafelip@gmail.com>
2019-06-11 20:29:28 +0200

    layout buffer done

--- Hazel/src/Hazel/Renderer/Buffer.h
+++ Hazel/src/Hazel/Renderer/Buffer.h
@@ -12,18 +12,18 @@ namespace Hazel
 	{
 		switch (type)
 		{
-			case ShaderDataType::None:	return 0;
-			case ShaderDataType::Float:	return 4;
+			case ShaderDataType::None:		return 0;
+			case ShaderDataType::Float:		return 4;
 			case ShaderDataType::Float2:	return 4 * 2;
 			case ShaderDataType::Float3:	return 4 * 3;
 			case ShaderDataType::Float4:	return 4 * 4;
-			case ShaderDataType::Mat3:	return 4 * 3 * 3;
-			case ShaderDataType::Mat4:	return 4 * 4 * 4;
-			case ShaderDataType::Int:	return 4;
-			case ShaderDataType::Int2:	return 4 * 2;
-			case ShaderDataType::Int3:	return 4 * 3;
-			case ShaderDataType::Int4:	return 4 * 4;
-			case ShaderDataType::Bool:	return 1;
+			case ShaderDataType::Mat3:		return 4 * 3 * 3;
+			case ShaderDataType::Mat4:		return 4 * 4 * 4;
+			case ShaderDataType::Int:		return 4;
+			case ShaderDataType::Int2:		return 4 * 2;
+			case ShaderDataType::Int3:		return 4 * 3;
+			case ShaderDataType::Int4:		return 4 * 4;
+			case ShaderDataType::Bool:		return 1;
 		}
 		HZ_CORE_ASSERT(false, "Unknown ShaderDataType!!!");
 		return 0;
@@ -35,12 +35,44 @@ namespace Hazel
 		ShaderDataType Type;
 		uint32_t Offset;
 		uint32_t Size;
+		bool Normalized;
 		
 		BufferElement() {}
 
-		BufferElement(ShaderDataType type, const std::string& name) :
-			Name(name), Type(type), Offset(0), Size(ShaderDataTypeSize(type))
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false) :
+			Name(name), Type(type), Offset(0), Size(ShaderDataTypeSize(type)), Normalized(normalized)
 		{}
+
+		uint32_t GetComponentCount() const
+		{
+			switch (Type)
+			{
+				case Hazel::ShaderDataType::Float:
+					return 1;
+				case Hazel::ShaderDataType::Float2:
+					return 2;
+				case Hazel::ShaderDataType::Float3:
+					return 3;
+				case Hazel::ShaderDataType::Float4:
+					return 4;
+				case Hazel::ShaderDataType::Mat3:
+					return 9;
+				case Hazel::ShaderDataType::Mat4:
+					return 16;
+				case Hazel::ShaderDataType::Int:
+					return 1;
+				case Hazel::ShaderDataType::Int2:
+					return 2;
+				case Hazel::ShaderDataType::Int3:
+					return 3;
+				case Hazel::ShaderDataType::Int4:
+					return 4;
+				case Hazel::ShaderDataType::Bool:
+					return 1;
+			}
+			HZ_CORE_ASSERT(false, "Unknown ShaderDataType!!!");
+			return 0;
+		}
 	};
 
 	class BufferLayout 
@@ -53,6 +85,7 @@ namespace Hazel
 		{
 			CalculateOffsetsAndStride();
 		}
+		inline const uint32_t GetStride() const { return m_Stride; }
 		inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
 		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
 		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
