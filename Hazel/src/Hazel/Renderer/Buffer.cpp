a2e3cbd44055e45c12b44fe2bcf5cce20c339dbe
aserra92 <aria.serrafelip@gmail.com>
2019-06-16 16:27:55 +0200

    rendererAPI and renderCommand

--- Hazel/src/Hazel/Renderer/Buffer.cpp
+++ Hazel/src/Hazel/Renderer/Buffer.cpp
@@ -10,10 +10,10 @@ namespace Hazel
 	{
 		switch (Renderer::GetAPI())
 		{
-		case RendererAPI::None:
+		case RendererAPI::API::None:
 			HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!!");
 			return nullptr;
-		case RendererAPI::OpenGL:
+		case RendererAPI::API::OpenGL:
 			return new OpenGLVertexBuffer(vertices, size);
 		}
 		HZ_CORE_ASSERT(false, "Unknown RendererAPI!!");
@@ -24,10 +24,10 @@ namespace Hazel
 	{
 		switch (Renderer::GetAPI())
 		{
-		case RendererAPI::None:
+		case RendererAPI::API::None:
 			HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!!");
 			return nullptr;
-		case RendererAPI::OpenGL:
+		case RendererAPI::API::OpenGL:
 			return new OpenGLIndexBuffer(indices, count);
 		}
 		HZ_CORE_ASSERT(false, "Unknown RendererAPI!!");
