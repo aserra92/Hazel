da01abbead50f0faf75db3e4f84f83271160c67c
aserra92 <aria.serrafelip@gmail.com>
2019-07-07 18:15:33 +0200

    Moving stuff from application to sandbox and move camera implementation

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -6,13 +6,9 @@
 #include "Hazel/Events/ApplicationEvent.h"
 
 #include "Hazel/ImGui/ImGuiLayer.h"
-#include "Hazel/Renderer/Shader.h"
-#include "Hazel/Renderer/Buffer.h"
-#include "Hazel/Renderer/VertexArray.h"
-#include "Hazel/Renderer/OrthographicCamera.h"
 
 namespace Hazel {
-	class HAZEL_API Application
+	class Application
 	{
 	public:
 		Application();
@@ -34,14 +30,6 @@ namespace Hazel {
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
-
-		std::shared_ptr<Shader> m_Shader;
-		std::shared_ptr<VertexArray> m_VertexArray;
-
-		std::shared_ptr<Shader> m_BlueShader;
-		std::shared_ptr<VertexArray> m_SquareVA;
-
-		OrthographicCamera m_Camera;
 		static Application* s_Instance;
 	};
 
