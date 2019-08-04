3320c48193edf826baac36b3965baacb15a8db13
aserra92 <aria.serrafelip@gmail.com>
2019-08-04 21:35:54 +0200

    uniforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -10,8 +10,7 @@ public:
 	ExampleLayer() :
 		Layer("Example"),
 		m_Camera(-1.6f, 1.6f, -0.9f, 0.9f),
-		m_CameraPosition(0.0f),
-		m_SquarePosition(0.0f)
+		m_CameraPosition(0.0f)
 	{
 		m_VertexArray.reset(Hazel::VertexArray::Create());
 
@@ -90,7 +89,7 @@ public:
 
 		m_Shader.reset(new Hazel::Shader(vertexSource, fragmentSource));
 
-		std::string blueShaderVertexSource = R"(
+		std::string flatColorShaderVertexSource = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -103,19 +102,21 @@ public:
 				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
-		std::string blueShaderFragmentSource = R"(
+		std::string flatColorShaderFragmentSource = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
 
+			uniform vec4 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = u_Color;
 			}
 		)";
 
-		m_BlueShader.reset(new Hazel::Shader(blueShaderVertexSource, blueShaderFragmentSource));
+		m_FlatColorShader.reset(new Hazel::Shader(flatColorShaderVertexSource, flatColorShaderFragmentSource));
 	}
 
 	void OnUpdate(Hazel::TimeStep ts) override
@@ -141,12 +142,20 @@ public:
 		m_Camera.SetRotation(m_CameraRotation);
 		Hazel::Renderer::BeginScene(m_Camera);
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
+
 		for (int y = 0; y < 20; y++)
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Hazel::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				if (x % 2 == 0)
+					m_FlatColorShader->UploadUniformFloat4("u_Color",redColor);
+				else
+					m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
+				Hazel::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 
 		Hazel::Renderer::Submit(m_Shader, m_VertexArray);
@@ -165,7 +174,7 @@ private:
 	std::shared_ptr<Hazel::Shader> m_Shader;
 	std::shared_ptr<Hazel::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Hazel::Shader> m_BlueShader;
+	std::shared_ptr<Hazel::Shader> m_FlatColorShader;
 	std::shared_ptr<Hazel::VertexArray> m_SquareVA;
 
 	Hazel::OrthographicCamera m_Camera;
