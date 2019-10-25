b291a05a0fa61b457aa62c55ae6c1961b97f73b6
aserra92 <aria.serrafelip@gmail.com>
2019-10-25 02:00:33 +0200

    uniforms and transform

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -3,7 +3,6 @@
 #include "imgui/imgui.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include "Platform/OpenGL/OpenGLShader.h"
 
 Sandbox2D::Sandbox2D():
 	Layer("Sandbox2D"),
@@ -27,7 +26,8 @@ void Sandbox2D::OnUpdate(Hazel::TimeStep ts)
 	Hazel::RenderCommand::Clear();
 
 	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });	
+	Hazel::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Hazel::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.2f, 1.0f });
 	Hazel::Renderer2D::EndScene();
 }
 
