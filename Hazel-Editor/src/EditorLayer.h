a53682ab0fb0e14284d951c347872da5ce603775
Aria Serra <aria.serrafelip@gmail.com>
2020-06-11 20:16:08 +0200

    Adding hazel editor

---
+++ Hazel-Editor/src/EditorLayer.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Hazel.h"
+
+namespace Hazel
+{
+	class EditorLayer : public Layer
+	{
+	public:
+		EditorLayer();
+		virtual ~EditorLayer() = default;
+
+		virtual void OnAttach() override;
+		virtual void OnDetach() override;
+
+		void OnUpdate(TimeStep ts) override;
+		void OnEvent(Event& e) override;
+		virtual void OnImGuiRender() override;
+	private:
+		OrthographicCameraController m_CameraController;
+
+		Ref<VertexArray> m_SquareVA;
+		Ref<Shader> m_FlatColorShader;
+		Ref<FrameBuffer> m_FrameBuffer;
+
+		Ref<Texture2D> m_CheckerboardTexture;
+
+		glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
+	};
+}
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -53,11 +53,6 @@ void Sandbox2D::OnAttach()
 	m_Particle.Position = { 0.0f, 0.0f };
 
 	m_CameraController.SetZoomLevel(5.0f);
-
-	Hazel::FrameBufferSpecification fbSpec;
-	fbSpec.Width = 1280;
-	fbSpec.Height = 720;
-	m_FrameBuffer = Hazel::FrameBuffer::Create(fbSpec);
 }
 
 void Sandbox2D::OnDetach()
@@ -74,7 +69,6 @@ void Sandbox2D::OnUpdate(Hazel::TimeStep ts)
 	Hazel::Renderer2D::ResetStats();
 	{
 		HZ_PROFILE_SCOPE("Renderer Prep");
-		m_FrameBuffer->Bind();
 		Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		Hazel::RenderCommand::Clear();
 	}
@@ -144,7 +138,6 @@ void Sandbox2D::OnUpdate(Hazel::TimeStep ts)
 
 		m_ParticleSystem.OnUpdate(ts);
 		m_ParticleSystem.OnRender(m_CameraController.GetCamera());
-		m_FrameBuffer->Unbind();
 	}
 }
 
@@ -156,61 +149,7 @@ void Sandbox2D::OnEvent(Hazel::Event& e)
 void Sandbox2D::OnImGuiRender()
 {
 	HZ_PROFILE_FUNCTION();
-	static bool dockspaceOpen = true;
-	static bool opt_fullscreen_persistant = true;
-	static ImGuiDockNodeFlags opt_flags = ImGuiDockNodeFlags_None;
-	bool opt_fullscreen = opt_fullscreen_persistant;
-
-	// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-	// because it would be confusing to have two docking targets within each others.
-	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-	if (opt_fullscreen)
-	{
-		ImGuiViewport* viewport = ImGui::GetMainViewport();
-		ImGui::SetNextWindowPos(viewport->Pos);
-		ImGui::SetNextWindowSize(viewport->Size);
-		ImGui::SetNextWindowViewport(viewport->ID);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-	}
-
-	// When using ImGuiDockNodeFlags_PassthruDockspace, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
-	if (opt_flags & ImGuiDockNodeFlags_PassthruDockspace)
-		window_flags |= ImGuiWindowFlags_NoBackground;
-
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-	ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
-	ImGui::PopStyleVar();
-
-	if (opt_fullscreen)
-		ImGui::PopStyleVar(2);
-
-	// Dockspace
-	ImGuiIO& io = ImGui::GetIO();
-	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-	{
-		ImGuiID dockspace_id = ImGui::GetID("MyDockspace");
-		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), opt_flags);
-	}
-
-	if (ImGui::BeginMenuBar())
-	{
-		if (ImGui::BeginMenu("File"))
-		{
-			// Disabling fullscreen would allow the window to be moved to the front of other windows, 
-			// which we can't undo at the moment without finer window depth/z control.
-			//ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen_persistant);
-
-			if (ImGui::MenuItem("Exit"))
-				Hazel::Application::Get().Close();
-			ImGui::EndMenu();
-		}
-
-		ImGui::EndMenuBar();
-	}
-
+	
 	ImGui::Begin("Settings");
 	auto stats = Hazel::Renderer2D::GetStats();
 	ImGui::Text("Renderer2D Stats:");
@@ -218,12 +157,6 @@ void Sandbox2D::OnImGuiRender()
 	ImGui::Text("Quads: %d", stats.QuadCount);
 	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
 	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
-	ImGui::ColorEdit4("Square color", glm::value_ptr(m_SquareColor));
-
-	uint32_t textureId = m_FrameBuffer->GetColorAttachmentRendererID();
-	ImGui::Image((void*)textureId, ImVec2{ 1280.0f, 720.0f });
-
-	ImGui::End();
 
 	ImGui::End();
 }
