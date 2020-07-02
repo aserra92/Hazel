f174a128da883b877aff804faedd0b721ea82deb
Aria Serra <aria.serrafelip@gmail.com>
2020-07-02 13:57:30 +0200

    fixing negative framebuffer size

--- Hazel/src/Platform/OpenGL/OpenGLFrameBuffer.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLFrameBuffer.cpp
@@ -4,6 +4,7 @@
 #include <glad/glad.h>
 
 namespace Hazel {
+	static const uint32_t s_MaxFrameBufferSize = 8192;
 	OpenGLFrameBuffer::OpenGLFrameBuffer(const FrameBufferSpecification& spec) :
 		m_Specification(spec)
 	{
@@ -60,6 +61,11 @@ namespace Hazel {
 
 	void OpenGLFrameBuffer::Resize(uint32_t width, uint32_t height)
 	{
+		if (width == 0 || height == 0 || width > s_MaxFrameBufferSize || height > s_MaxFrameBufferSize)
+		{
+			HZ_CORE_WARN("Attempted to resize Framebuffer to {0}, {1}", width, height);
+			return;
+		}
 		m_Specification.Width = width;
 		m_Specification.Height = height;
 
