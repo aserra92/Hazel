871f2454e456c79f5f6d666821ac801c1f6bf1be
Aria Serra <aria.serrafelip@gmail.com>
2021-01-22 20:24:54 +0100

    refactor framebuffer

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -26,6 +26,7 @@ namespace Hazel
 		m_CheckerboardTexture = Texture2D::Create("assets/textures/CheckerBoard.png");
 
 		FrameBufferSpecification fbSpec;
+		fbSpec.Attachments = { FramebufferTextureFormat::RGBA8, FramebufferTextureFormat::Depth };
 		fbSpec.Width = 1280;
 		fbSpec.Height = 720;
 		m_FrameBuffer = FrameBuffer::Create(fbSpec);
